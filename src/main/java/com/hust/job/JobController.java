743487a44c78db7368d483169949a15fef927314
phuongtrannam <trannamphuonga197@gmail.com>
2020-05-19 08:54:09 +0700

    add method to job controller and service

--- src/main/java/com/hust/job/JobController.java
+++ src/main/java/com/hust/job/JobController.java
@@ -1,8 +1,105 @@
 package com.hust.job;
 
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
+
+    @Autowired
+    private JobService jobService;
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/basic_search_job", method = RequestMethod.POST)
+    public JSONObject basicSearchJob(@RequestBody Map<String, Object> payload) throws Exception {
+        String queryContent = payload.get("queryContent").toString();
+        return jobService.basicSearchJob(queryContent);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_job_info", method = RequestMethod.POST)
+    public JSONObject getJobInfo(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        return jobService.getJobInfo(idJob);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/advanced_search_job", method = RequestMethod.POST)
+    public JSONObject advancedSearchJob(@RequestBody Map<String, Object> payload) throws Exception {
+        String queryContent = payload.get("queryContent").toString();
+        String idLocation = payload.get("idLocation").toString();
+        String idIndustry = payload.get("idIndustry").toString();
+        String salary = payload.get("salary").toString();
+        return jobService.advancedSearchJob(queryContent, idLocation, idIndustry, salary);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_related_job", method = RequestMethod.POST)
+    public JSONObject getJobRelated(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        return jobService.getJobRelated(idJob);
+    }
     
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_skill_required", method = RequestMethod.POST)
+    public JSONObject getSkillRequired(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        return jobService.getSkillRequired(idJob);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_job_demand_by_period_of_time", method = RequestMethod.POST)
+    public JSONObject getJobDemandByPeriodOfTime(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getJobDemandByPeriodOfTime(idJob, idLocation);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_average_salary", method = RequestMethod.POST)
+    public JSONObject getAverageSalary(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getAverageSalary(idJob, idLocation);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_job_demand_in_sub_region", method = RequestMethod.POST)
+    public JSONObject getJobDemandInSubRegion(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getJobDemandInSubRegion(idJob, idLocation);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_average_salary_in_sub_region", method = RequestMethod.POST)
+    public JSONObject getAverageSalaryInSubRegion(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getAverageSalaryInSubRegion(idJob, idLocation);
+    }
+    
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_job_demand_by_age", method = RequestMethod.POST)
+    public JSONObject getJobDemandByAge(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getJobDemandByAge(idJob, idLocation);
+    }
+
+    @CrossOrigin(origins =  "http://localhost:4200")
+    @RequestMapping(value = "/jobs/get_job_demand_by_literacy", method = RequestMethod.POST)
+    public JSONObject getJobDemandByLiteracy(@RequestBody Map<String, Object> payload) throws Exception {
+        String idJob = payload.get("idJob").toString();
+        String idLocation = payload.get("idLocation").toString();
+        return jobService.getJobDemandByLiteracy(idJob, idLocation);
+    }
+
 }
--- src/main/java/com/hust/job/JobService.java
+++ src/main/java/com/hust/job/JobService.java
@@ -86,31 +86,173 @@ public class JobService {
         return jsonObject;
     }
 
+    public JSONObject getJobRelated(String idJob){
 
-
-
-
-
-
-
-
-    public JSONObject getBusinessLinesOfTheCompany(String id) {
         final JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", id);
-        jsonObject.put("description", "The business lines of the company");
+        jsonObject.put("description", "The job realted");
 
-        final JSONArray businessLines = new JSONArray();
+        final JSONArray jobArr = new JSONArray();
 
-        List<Object[]> list = companyRepository.getBusinessLinesOfTheCompany(id);
-        System.out.println(id);
+        List<Object[]> list = jobRepository.getJobRelated(idJob);
         for(Object[] ob : list){
-            HashMap<String, String> industryObject = new HashMap<String, String>();
-            industryObject.put("id", ob[0].toString());
-            industryObject.put("name", ob[1].toString());
-            
-            businessLines.add(industryObject);
+            HashMap<String, String> jobObject = new HashMap<String, String>();
+            jobObject.put("id", ob[0].toString());
+            jobObject.put("name", ob[1].toString());
+            jobObject.put("averageSalary", ob[2].toString());
+            jobObject.put("maxSalary", ob[2].toString());
+            jobObject.put("minSalary", ob[2].toString());
+            jobObject.put("numJob", ob[1].toString());
+            // jobObject.put("gender", ob[4].toString());
+            // jobObject.put("jobType", ob[5].toString());
+            jobArr.add(jobObject);
         }
-        jsonObject.put("result", businessLines);
+        jsonObject.put("result", jobArr);
+        return jsonObject;
+    }
+
+    public JSONObject getSkillRequired(String idJob){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The job info");
+
+        final JSONArray skillArr = new JSONArray();
+
+        List<Object[]> list = jobRepository.getSkillRequired(idJob);
+        for(Object[] ob : list){
+            HashMap<String, String> skillObject = new HashMap<String, String>();
+            skillObject.put("id", ob[0].toString());
+            skillObject.put("name", ob[1].toString());
+            skillObject.put("description", ob[2].toString());
+            
+            skillArr.add(skillObject);
+        }
+        jsonObject.put("result", skillArr);
+        return jsonObject;
+    }
+
+    public JSONObject getJobDemandByPeriodOfTime(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The job demand by period of time");
+
+        final JSONArray periods = new JSONArray();
+
+        List<Object[]> list = jobRepository.getJobDemandByPeriodOfTime(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> period = new HashMap<String, String>();
+            period.put("timestamp", ob[0].toString());
+            period.put("numJob", ob[1].toString());
+            period.put("growth", ob[2].toString());
+            
+            periods.add(period);
+        }
+        jsonObject.put("result", periods);
+        return jsonObject;
+    }
+
+    public JSONObject getAverageSalary(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The average salary");
+
+        final JSONArray periods = new JSONArray();
+
+        List<Object[]> list = jobRepository.getAverageSalary(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> period = new HashMap<String, String>();
+            period.put("timestamp", ob[0].toString());
+            period.put("averageSalay", ob[1].toString());
+            period.put("growth", ob[2].toString());
+            
+            periods.add(period);
+        }
+        jsonObject.put("result", periods);
+        return jsonObject;
+    }
+
+    public JSONObject getJobDemandInSubRegion(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The job demand in sub region");
+
+        final JSONArray subRegions = new JSONArray();
+
+        List<Object[]> list = jobRepository.getJobDemandInSubRegion(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> subRegion = new HashMap<String, String>();
+            subRegion.put("timestamp", ob[0].toString());
+            subRegion.put("idSubRegion", ob[0].toString());
+            subRegion.put("regionName", ob[0].toString());
+            subRegion.put("numJob", ob[1].toString());
+            subRegion.put("growth", ob[2].toString());
+            
+            subRegions.add(subRegion);
+        }
+        jsonObject.put("result", subRegions);
+        return jsonObject;
+    }
+
+    public JSONObject getAverageSalaryInSubRegion(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The average salary in sub region");
+
+        final JSONArray subRegions = new JSONArray();
+
+        List<Object[]> list = jobRepository.getAverageSalaryInSubRegion(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> subRegion = new HashMap<String, String>();
+            subRegion.put("timestamp", ob[0].toString());
+            subRegion.put("idSubRegion", ob[0].toString());
+            subRegion.put("regionName", ob[0].toString());
+            subRegion.put("averageSalary", ob[1].toString());
+            subRegion.put("growth", ob[2].toString());
+            
+            subRegions.add(subRegion);
+        }
+        jsonObject.put("result", subRegions);
+        return jsonObject;
+    }
+
+    public JSONObject getJobDemandByAge(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The job demand by age");
+
+        final JSONArray ageRanges = new JSONArray();
+
+        List<Object[]> list = jobRepository.getJobDemandByAge(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> ageRange = new HashMap<String, String>();
+            ageRange.put("timestamp", ob[0].toString());
+            ageRange.put("numJob", ob[0].toString());
+            ageRange.put("gender", ob[0].toString());
+            ageRange.put("age", ob[1].toString());
+            
+            ageRanges.add(ageRange);
+        }
+        jsonObject.put("result", ageRanges);
+        return jsonObject;
+    }
+
+    public JSONObject getJobDemandByLiteracy(String idJob, String idLocation){
+
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("description", "The job demand by literacy");
+
+        final JSONArray literacies = new JSONArray();
+
+        List<Object[]> list = jobRepository.getJobDemandByLiteracy(idJob, idLocation);
+        for(Object[] ob : list){
+            HashMap<String, String> literacy = new HashMap<String, String>();
+            literacy.put("timestamp", ob[0].toString());
+            literacy.put("numJob", ob[0].toString());
+            literacy.put("literacy", ob[0].toString());
+            literacy.put("growth", ob[1].toString());
+            
+            literacies.add(literacy);
+        }
+        jsonObject.put("result", literacies);
         return jsonObject;
     }
 }
